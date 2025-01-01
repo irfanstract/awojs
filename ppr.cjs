322abbd9a5dcab1d0f3ba0ac21f88759c41a3fc7
Bot <ci@example.com>
2025-01-01 03:29:26 +0000

    After These Update

--- ppr.cjs
+++ ppr.cjs
@@ -23,6 +23,15 @@ runCmd(`git fetch origin main`) ;
 
 
 /**
+ * @type {| { mkc: false, forceUpdateRemote?: false, } | { mkc?: true, forceUpdateRemote: boolean, }}
+ */
+const {
+  mkc = true,
+  forceUpdateRemote: forcePushToRemoteMain = mkc ? true : false ,
+} = { mkc: true, forceUpdateRemote: true, };
+
+
+/**
  * ensure there's no uncommitted chgs at time of this running
  * 
  */
@@ -47,6 +56,22 @@ runCmd(`git rebase --rebase-merges --no-autostash --reapply-cherry-picks HEAD~0`
   runCmd(`git fetch origin main`) ;
   
   runCmd(`git reset --soft "FETCH_HEAD~0" `) ;
+  runCmd(`git status`) ;
+
+  if (mkc) {
+    ;
+
+    runCmd(`git commit --allow-empty -m "after these WIP(s)"`) ;
+    runCmd(`git status`) ;
+
+    if (forcePushToRemoteMain) {
+      ;
+
+      runCmd(`git push origin HEAD:refs/heads/main`) ;
+
+    }
+
+  }
 
 }
 
